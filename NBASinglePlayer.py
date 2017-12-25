63f88f75695dc4dc5cc5ac39e70870fc5c37da3d
SACHIN JOSE <sachinjose16@gmail.com>
2017-12-25 22:52:57 +0530

    Python Pgm to extract details of a single player from the NBA website

--- NBASinglePlayer.py
+++ NBASinglePlayer.py
@@ -3,24 +3,23 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-def get_player_details():
-	driver = webdriver.PhantomJS(executable_path = r'/Users/sachinjose/Downloads/phantomjs/bin/phantomjs')
-	url="http://www.nba.com/players/alex/abrines/203518"
-	driver.get(url)
-	soup=BeautifulSoup(driver.page_source,'lxml')
-	born="BORN "
-	b_span=soup.find('span',string="BORN")
-	for span in b_span.findNextSiblings():	
-		born = born + span.text
-	height=""
-	h_span=soup.find('section',class_="nba-player-vitals__top-left small-6")
-	for a in h_span.find_all('p'):
-		height=height+a.text
-	weight=""
-	w_span=soup.find('section',class_="nba-player-vitals__top-right small-6")
-	for a in w_span.find_all('p'):
-		weight=weight+a.text
-	print weight,born,height
-	return null
 
-get_player_details()
+driver = webdriver.PhantomJS(executable_path = r'/Users/sachinjose/Downloads/phantomjs/bin/phantomjs')
+url="http://www.nba.com/players/alex/abrines/203518"
+driver.get(url)
+soup=BeautifulSoup(driver.page_source,'lxml')
+born="BORN "
+b_span=soup.find('span',string="BORN")
+for span in b_span.findNextSiblings():	
+	born = born + span.text
+print born
+height=""
+h_span=soup.find('section',class_="nba-player-vitals__top-left small-6")
+for a in h_span.find_all('p'):
+	height=height+a.text
+print height
+weight=""
+w_span=soup.find('section',class_="nba-player-vitals__top-right small-6")
+for a in w_span.find_all('p'):
+	weight=weight+a.text
+print weight
